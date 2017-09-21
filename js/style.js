51d0a39034284241ab404bf75e860ea00e91e9c7
Oskar85 <alexbal2010@gmail.com>
2017-09-21 13:27:26 +0300

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,11 +1,16 @@
-	$(document).ready(function(){
-	  $('.bxslider').bxSlider({
-	  	auto: true,
-	  	controls: false
-	  	
-	  });
-	  $(document).ready(function() {
+	$(document).ready(function() {
+	    $('.bxslider').bxSlider({
+	        auto: true,
+	        controls: false
+
+	    });
+
+
+	});
+
+	$(document).ready(function() {
 		$(".hamburger").click(function(){
-    		$(".menu").toggle(500);
+    	$(".menu").toggle(500);
 	});
-	});
+
+});
